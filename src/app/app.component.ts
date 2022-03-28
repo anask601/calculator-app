080c118a625774e4eeff4abde9965ed19de0f700
anask601 <anask60155@gmail.com>
2022-03-28 12:19:32 +0530

    feat: add operand to enteredvalue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculator-app';
+  OnScreen() {}
+
+  OnEnteredValue(num: string) {
+    console.log(num);
+  }
 }
