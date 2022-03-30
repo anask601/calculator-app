0f2f3ccb85981d431a753adca06f5143ee3fc904
anask601 <anask60155@gmail.com>
2022-03-30 18:06:25 +0530

    feat: bugs fixed

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -9,10 +9,17 @@ import { buttons } from '../button';
 export class CalculatorComponent implements OnInit {
   buttons = buttons;
   parentValue: string = '';
+  actionPerform = false;
+
   constructor() {}
 
   ngOnInit(): void {}
   showWhichButtonHasBeenClicked(e: any) {
+    if (this.actionPerform) {
+      this.actionPerform = false;
+      this.parentValue = '';
+    }
+
     switch (e.action) {
       case 'clearAll': {
         this.clearInput();
@@ -31,9 +38,7 @@ export class CalculatorComponent implements OnInit {
 
   calculateResult() {
     console.log(this.parentValue);
-    if (this.parentValue === '') {
-      return;
-    }
     this.parentValue = eval(this.parentValue);
+    this.actionPerform = true;
   }
 }
